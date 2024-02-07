3274c56681fee010f01e9670ef028854482a9c7f
chiaxiu <chiaxiu@gmail.com>
2024-02-07 23:22:50 +0800

    part 2.18-2.20 submission

--- part2/2.18-2.20_countries/src/App.jsx
+++ part2/2.18-2.20_countries/src/App.jsx
@@ -9,7 +9,49 @@ const Filter = ({filter, handleFilterChange}) => {
   )
 }
 
-const Content = ({filteredCountries}) => {
+const Button = ({handleButtonClick, country}) => {
+  return(
+    <button onClick={() => handleButtonClick(country)}>Show</button>
+  )
+}
+
+const Country = ({country}) => {
+  const [temperature, setTemperature] = useState(null)
+  const [windSpeed, setWindSpeed] = useState(null)
+  const [icon, setIcon] = useState(null)
+  const [iconUrl, setIconUrl] = useState('')
+  const api_key = import.meta.env.VITE_SOME_KEY
+
+  axios
+    .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${country.latlng[0]}&lon=${country.latlng[1]}&units=metric&exclude=minutely,hourly,daily&appid=${api_key}`)
+    .then(response => {
+      setTemperature(response.data.current.temp)
+      setWindSpeed(response.data.current.wind_speed)
+      setIcon(response.data.current.weather[0].icon)
+      setIconUrl(`https://openweathermap.org/img/wn/${icon}@2x.png`)
+    })
+
+  return(
+    <div key={country.name.common}>
+      <h1>{country.name.common}</h1>
+      <p>capital: {country.capital}</p>
+      <p>area: {country.area}</p>
+      <h3>languages:</h3>  
+      <ul>
+        {Object.values(country.languages).map(language => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt={country.flags.alt} />
+      <h2>Weather in {country.capital}</h2>
+      <p>temperature {temperature} Celcius</p>
+      <img src={iconUrl} alt="image not available"/>
+      <p>wind {windSpeed} m/s</p>
+    </div>
+  )
+}
+
+const Content = ({filteredCountries, handleButtonClick, selectedCountry}) => {
   console.log(filteredCountries.length)
 
   if (filteredCountries.length > 10) {
@@ -19,38 +61,34 @@ const Content = ({filteredCountries}) => {
       </div>
     )
   } if (filteredCountries.length === 1){ 
-    return(
-      <div>
-        {filteredCountries.map(country => 
-          <div key={country.name.common}>
-            <h1>{country.name.common}</h1>
-            <p>capital: {country.capital}</p>
-            <p>area: {country.area}</p>
-            <h3>languages:</h3>  
-            <ul>
-              {Object.values(country.languages).map(language => (
-                <li key={language}>{language}</li>
-              ))}
-            </ul>
-            <img src={country.flags.png} alt={country.flags.alt} />   
-          </div>
-          )}
-      </div>
-    )
-  }
+      const country = filteredCountries[0]
+      
       return(
-        <div>
-          {filteredCountries.map(country => 
-            <p key={country.name.common}>
-              {country.name.common}
-            </p>)}
-        </div>
+        <Country country={country}/>            
       )
+    } if (selectedCountry !== null) {
+        const country = selectedCountry
+
+        return(
+          <Country country={country}/>              
+        )
+      }
+        return(
+          <div>
+            {filteredCountries.map(country => 
+              <div key={country.name.common}>
+                <>{country.name.common} </>
+                <Button handleButtonClick={handleButtonClick} country={country}/>
+              </div>
+            )}
+          </div>
+        )
 }
 
 function App() {
   const [allCountries, setAllCountries] = useState([])
   const [filter, setFilter] = useState('')
+  const [selectedCountry, setSelectedCountry] = useState(null)
 
   useEffect(() => {
     axios
@@ -62,6 +100,11 @@ function App() {
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
+    setSelectedCountry(null) // Clear selected country when filtering
+  }
+
+  const handleButtonClick = (country) => {
+    setSelectedCountry(country)
   }
 
   const filteredCountries = allCountries.filter(
@@ -71,8 +114,11 @@ function App() {
   return (
    <div>
       <Filter filter={filter} handleFilterChange={handleFilterChange}/>
-      <Content filteredCountries={filteredCountries}/>
-      
+      <Content 
+        filteredCountries={filteredCountries}
+        handleButtonClick={handleButtonClick}
+        selectedCountry={selectedCountry}
+      />  
    </div> 
   )
 }
