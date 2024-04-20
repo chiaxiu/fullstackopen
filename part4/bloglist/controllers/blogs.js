befe31c2eedd7b937dfcbbc698450c1a6705587a
chiaxiu <chiaxiu@gmail.com>
2024-04-20 15:26:39 +0800

    part5 5.5-5.12

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -34,6 +34,7 @@ blogsRouter.put("/:id", async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: body.user,
   };
 
   if (body.likes !== undefined) {
