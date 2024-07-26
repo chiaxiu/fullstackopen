c538b774d12e54d235c05894ac0b5b16b1eb3a2f
chiaxiu <chiaxiu@gmail.com>
2024-07-26 21:53:12 +0800

    part12.7-12.12

--- part12/part12-containers-applications/todo-app/todo-backend/routes/index.js
+++ part12/part12-containers-applications/todo-app/todo-backend/routes/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-const configs = require('../util/config')
+const configs = require('../util/config');
+const redis = require('../redis');
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
+  visits++;
 
   res.send({
     ...configs,
     visits
   });
+});
+
+router.get('/statistics', async (req, res) => {
+  try {
+    const addedTodos = await redis.getAsync('added_todos');
+    res.json({
+      added_todos: addedTodos ? parseInt(addedTodos) : 0
+    });
+  } catch (error) {
+    console.error('Error fetching statistics:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
--- part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Todo } = require('../mongo');
 const router = express.Router();
+const redis = require('../redis');
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -14,6 +15,15 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false
   });
+
+  try {
+    const currentCount = await redis.getAsync('added_todos');
+    const newCount = currentCount ? parseInt(currentCount) + 1 : 1;
+    await redis.setAsync('added_todos', newCount.toString());
+  } catch (error) {
+    console.error('Error incrementing todo counter:', error);
+  }
+
   res.send(todo);
 });
 
